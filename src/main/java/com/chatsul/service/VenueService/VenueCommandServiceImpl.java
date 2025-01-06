0b97e5323788a2ea755d2dba5c741807901f09fa
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-01-07 04:31:25 +0900

    :sparkles: feat: 매장 추가 기능 구현 #4

---
+++ src/main/java/com/chatsul/service/VenueService/VenueCommandServiceImpl.java
@@ -0,0 +1,41 @@
+package com.chatsul.service.VenueService;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.chatsul.domain.Venue;
+import com.chatsul.repository.VenueRepository;
+import com.chatsul.web.dto.VenueRequestDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class VenueCommandServiceImpl implements VenueCommandService {
+
+	private final VenueRepository venueRepository;
+	private final KakaoMapService kakaoMapService;
+
+	@Override
+	public Venue createVenue(VenueRequestDTO request) {
+		Map<String, Double> coordinates = kakaoMapService.getCoordinates(request.getAddress());
+
+		Double latitude = coordinates.get("latitude");
+		Double longitude = coordinates.get("longitude");
+
+		Venue venue = Venue.builder()
+			.name(request.getName())
+			.address(request.getAddress())
+			.phone(request.getPhone())
+			.latitude(latitude)
+			.longitude(longitude)
+			.account(request.getAccount())
+			.bank(request.getBank())
+			.build();
+
+		return venueRepository.save(venue);
+	}
+}
