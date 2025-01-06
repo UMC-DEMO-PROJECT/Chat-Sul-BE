529c4f790ceb4aeb622bc4c0973e15333e7562e9
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-01-07 04:30:20 +0900

    :sparkles: feat: 매장 엔티티 생성 #4

---
+++ src/main/java/com/chatsul/domain/Venue.java
@@ -0,0 +1,54 @@
+package com.chatsul.domain;
+
+import com.chatsul.domain.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Venue extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// 가게 이름
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	// 가게 주소
+	@Column(nullable = false, length = 100)
+	private String address;
+
+	// 가게 전화번호
+	@Column(nullable = false, length = 50)
+	private String phone;
+
+	// 가게 위도
+	@Column(nullable = false)
+	private Double latitude;
+
+	// 가게 경도
+	@Column(nullable = false)
+	private Double longitude;
+
+	// 가게 계좌
+	@Column(nullable = false, length = 50)
+	private String account;
+
+	// 가게 은행
+	@Column(nullable = false, length = 50)
+	private String bank;
+}
