8dbf24de873415aac3e484f5eea66b99017850c3
changyunyeong <sally0109277@naver.com>
2025-01-15 14:35:14 +0900

    ✨  feat: 사용자 인증 및 사용자 정보 저장 #15

--- src/main/java/com/chatsul/service/ReservationService/ReservationCommandService.java
+++ src/main/java/com/chatsul/service/ReservationService/ReservationCommandService.java
@@ -1,10 +1,11 @@
 package com.chatsul.service.ReservationService;
 
+import com.chatsul.domain.Member;
 import com.chatsul.domain.Reservation;
 import com.chatsul.web.dto.ReservationRequestDTO;
 
 public interface ReservationCommandService {
 
-    Reservation createReservation(ReservationRequestDTO.MakeReservationRequestDTO request, Long venueId);
-    void cancelReservation(Long reservationId, Long userId);
+    Reservation createReservation(ReservationRequestDTO.MakeReservationRequestDTO request, Long venueId, Member member);
+    void cancelReservation(Long reservationId, Member member);
 }
--- src/main/java/com/chatsul/service/ReservationService/ReservationQueryServiceImpl.java
+++ src/main/java/com/chatsul/service/ReservationService/ReservationQueryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.chatsul.service.ReservationService;
 
+import com.chatsul.apiPayload.code.status.ErrorStatus;
+import com.chatsul.apiPayload.exception.GeneralException;
 import com.chatsul.domain.Member;
 import com.chatsul.domain.Reservation;
 import com.chatsul.repository.MemberRepository;
@@ -19,16 +21,17 @@ import java.time.LocalTime;
 @Transactional(readOnly = true)
 public class ReservationQueryServiceImpl implements ReservationQueryService {
 
-    private final MemberRepository memberRepository;
     private final ReservationRepository reservationRepository;
 
     @Override
-    public Page<Reservation> getReservationList(Long userId, Integer page) {
-        Member member = memberRepository.findById(userId).get();
+    public Page<Reservation> getReservationList(Member member, Integer page) {
+        if (member == null || member.getId() == null) {
+            throw new GeneralException(ErrorStatus.MEMBER_NOT_FOUND);
+        }
         LocalDate currentDate = LocalDate.now();
 
         // 예약 날짜 순으로 정렬 (오름차순)
-        PageRequest pageRequest = PageRequest.of(page, 10, Sort.by(Sort.Direction.ASC, "reservationDate"));
+        PageRequest pageRequest = PageRequest.of(page, 6, Sort.by(Sort.Direction.ASC, "reservationDate"));
 
         Page<Reservation> reservationPage = reservationRepository.findByMemberAndReservationDateAfter
                 (member, currentDate, pageRequest);
