88a59886cc239f29e6da9c7fbd659c5709dae4e1
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-01-23 02:10:37 +0900

    :sparkles: feat: Menu 엔티티 생성 #30

---
+++ src/main/java/com/chatsul/domain/Menu.java
@@ -0,0 +1,37 @@
+package com.chatsul.domain;
+
+import com.chatsul.domain.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Menu extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// 메뉴 이미지 url
+	@Column(nullable = false)
+	private String imageUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "venue_id")
+	private Venue venue;
+}
