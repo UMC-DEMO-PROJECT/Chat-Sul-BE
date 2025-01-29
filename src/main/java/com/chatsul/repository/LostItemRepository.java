a755133a800a3e489b8c97a6bddbc49239ceaedd
rhkrqkq <rhkrqkq1025@gmail.com>
2025-01-29 15:21:23 +0900

    ♻️ feat: jpa메소드로 변경

--- src/main/java/com/chatsul/repository/LostItemRepository.java
+++ src/main/java/com/chatsul/repository/LostItemRepository.java
@@ -1,10 +1,10 @@
 package com.chatsul.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +13,9 @@ import com.chatsul.domain.LostItem;
 @Repository
 public interface LostItemRepository extends JpaRepository<LostItem, Long> {
 
+	List<LostItem> findTop6ByOrderByCreatedAtDesc();
+
 	Page<LostItem> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-	@Modifying
-	@Query("DELETE FROM LostItem l WHERE l.lostItemId = :id")
 	void deleteByLostItemId(@Param("id") Long id);
 }
