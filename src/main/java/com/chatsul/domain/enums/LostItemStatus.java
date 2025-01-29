16560093c16e81ec244b23c80b03355db5ce1d5c
changyunyeong <sally0109277@naver.com>
2025-01-29 22:09:49 +0900

    ✨  feat: LostItemStatus enum 설명 추가 #8

--- src/main/java/com/chatsul/domain/enums/LostItemStatus.java
+++ src/main/java/com/chatsul/domain/enums/LostItemStatus.java
@@ -1,6 +1,13 @@
 package com.chatsul.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum LostItemStatus {
-    Lost,
-    Found;
+    Lost("미수취"),
+    Found("완료");
+
+    private final String status;
 }
