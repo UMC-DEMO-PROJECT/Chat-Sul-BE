a3ef7f67abb51ecb4aca3d8586fde7f5239d5bc2
changyunyeong <sally0109277@naver.com>
2025-01-31 17:32:04 +0900

    ⚡️  feat: transactional 추가 #8

--- src/main/java/com/chatsul/service/LostItemService/LostItemQueryServiceImpl.java
+++ src/main/java/com/chatsul/service/LostItemService/LostItemQueryServiceImpl.java
@@ -18,9 +18,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Slf4j
 public class LostItemQueryServiceImpl implements LostItemQueryService {
     private final LostItemRepository lostItemRepository;
